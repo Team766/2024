stinkystinkman/MaroonFramework

OI buttons tutorial

src/main/java/com/team766/frc2020/OI.java
@@ -32,6 +32,10 @@ public class OI extends Procedure {
 			// of mechanisms when appropriate.
 			Robot.drive.setArcadeDrivePower(m_joystick0.getAxis(1), m_joystick1.getAxis(0));
 
+			if (m_joystick0.getButtonPressed(1)) {
+				context.startAsync(new Launch());
+			}
+
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}
 	}

Team766/2024

src/main/java/com/team766/robot/OI.java
@@ -35,6 +35,10 @@ public class OI extends Procedure {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
 			Robot.drive.setArcadeDrivePower(joystick0.getAxis(1), joystick1.getAxis(0));
+
+			if (joystick0.getButtonPressed(1)) {
+				context.startAsync(new Launch());
+			}
 		}
 	}
 }