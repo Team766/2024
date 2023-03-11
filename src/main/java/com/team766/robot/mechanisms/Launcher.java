stinkystinkman/MaroonFramework

Launcher tutorial code

src/main/java/com/team766/frc2020/mechanisms/Launcher.java
@@ -1,0 +1,19 @@
+package com.team766.frc2020.mechanisms;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.RobotProvider;
+import com.team766.hal.SolenoidController;
+
+public class Launcher extends Mechanism {
+	private SolenoidController m_pusher;
+
+	public Launcher() {
+		m_pusher = RobotProvider.instance.getSolenoid("launch");
+	}
+
+	public void setPusher(boolean extended) {
+		checkContextOwnership();
+
+		m_pusher.set(extended);
+	}
+}

src/main/java/com/team766/frc2020/procedures/Launch.java
@@ -1,0 +1,19 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.frc2020.Robot;
+
+public class Launch extends Procedure {
+	public void run(Context context) {
+		context.takeOwnership(Robot.launcher);
+
+		Robot.launcher.setPusher(true);
+
+		context.waitForSeconds(0.25);
+
+		Robot.launcher.setPusher(false);
+
+		context.waitForSeconds(0.25);
+	}
+}

Team766/2024

src/main/java/com/team766/robot/mechanisms/Launcher.java
@@ -1,0 +1,19 @@
+package com.team766.robot.mechanisms;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.RobotProvider;
+import com.team766.hal.SolenoidController;
+
+public class Launcher extends Mechanism {
+	private SolenoidController pusher;
+
+	public Launcher() {
+		pusher = RobotProvider.instance.getSolenoid("launch");
+	}
+
+	public void setPusher(boolean extended) {
+		checkContextOwnership();
+
+		pusher.set(extended);
+	}
+}

src/main/java/com/team766/robot/procedures/Launch.java
@@ -1,0 +1,19 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.Robot;
+
+public class Launch extends Procedure {
+	public void run(Context context) {
+		context.takeOwnership(Robot.launcher);
+
+		Robot.launcher.setPusher(true);
+
+		context.waitForSeconds(0.25);
+
+		Robot.launcher.setPusher(false);
+
+		context.waitForSeconds(0.25);
+	}
+}