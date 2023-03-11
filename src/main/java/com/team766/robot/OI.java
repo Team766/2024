blaisegassend/MaroonFramework

Demo of a PID-controlled elevator mechanism

src/main/java/com/team766/robot/OI.java
@@ -28,7 +28,12 @@ public class OI extends Procedure {
 		while (true) {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			
+			if (joystick2.getButtonPressed(1)) {
+				context.startAsync(new IncrementElevatorPosition());
+			}
+			if (joystick2.getButtonPressed(2)) {
+				context.startAsync(new DecrementElevatorPosition());
+			}
 
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}

Team766/2024

src/main/java/com/team766/robot/OI.java
@@ -33,6 +33,13 @@ public class OI extends Procedure {
 
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
+
+			if (joystick2.getButton(1)) {
+				context.startAsync(new IncrementElevatorPosition());
+			}
+			if (joystick2.getButton(2)) {
+				context.startAsync(new DecrementElevatorPosition());
+			}
 		}
 	}
 }