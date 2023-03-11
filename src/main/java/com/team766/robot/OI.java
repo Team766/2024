stinkystinkman/MaroonFramework

Log OI state tutorial code

src/main/java/com/team766/frc2020/OI.java
@@ -29,7 +29,13 @@ public class OI extends Procedure {
 		while (true) {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			
+			log("J0 A0: " + m_joystick0.getAxis(0) +
+			    "  J0 A1: " + m_joystick0.getAxis(1) +
+			    "  J1 A0: " + m_joystick1.getAxis(0) +
+			    "  J1 A1: " + m_joystick1.getAxis(1) +
+			    "  J0 B1: " + m_joystick0.getButton(1) +
+			    "  J0 B2: " + m_joystick0.getButton(2) +
+			    "  J0 B3: " + m_joystick0.getButton(3));
 
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}

Team766/2024

src/main/java/com/team766/robot/OI.java
@@ -33,6 +33,13 @@ public class OI extends Procedure {
 
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
+			log("J0 A0: " + joystick0.getAxis(0) +
+			    "  J0 A1: " + joystick0.getAxis(1) +
+			    "  J1 A0: " + joystick1.getAxis(0) +
+			    "  J1 A1: " + joystick1.getAxis(1) +
+			    "  J0 B1: " + joystick0.getButton(1) +
+			    "  J0 B2: " + joystick0.getButton(2) +
+			    "  J0 B3: " + joystick0.getButton(3));
 		}
 	}
 }