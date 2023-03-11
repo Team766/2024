stinkystinkman/MaroonFramework

Example code for running procedures asynchronously

src/main/java/com/team766/frc2020/procedures/MyAutonomous.java
@@ -1,0 +1,15 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.framework.LaunchedContext;
+
+public class MyAutonomous extends Procedure {
+
+	public void run(Context context) {
+		LaunchedContext driveProcedure = context.startAsync(new DriveStraight());
+		LaunchedContext armProcedure = context.startAsync(new RaiseArm());
+		context.waitFor(driveProcedure, armProcedure);
+	}
+
+}

src/main/java/com/team766/frc2020/procedures/RaiseArm.java
@@ -1,0 +1,11 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+
+public class RaiseArm extends Procedure {
+
+	public void run(Context context) {
+	}
+
+}

Team766/2024

src/main/java/com/team766/robot/procedures/MyAutonomous.java
@@ -1,0 +1,15 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.framework.LaunchedContext;
+
+public class MyAutonomous extends Procedure {
+
+	public void run(Context context) {
+		LaunchedContext driveProcedure = context.startAsync(new DriveStraight());
+		LaunchedContext armProcedure = context.startAsync(new RaiseArm());
+		context.waitFor(driveProcedure, armProcedure);
+	}
+
+}

src/main/java/com/team766/robot/procedures/RaiseArm.java
@@ -1,0 +1,11 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+
+public class RaiseArm extends Procedure {
+
+	public void run(Context context) {
+	}
+
+}