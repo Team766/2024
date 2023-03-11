stinkystinkman/MaroonFramework

Drive encoders and DriveDistance

src/main/java/com/team766/frc2020/mechanisms/Drive.java
@@ -1,16 +1,24 @@
 package com.team766.frc2020.mechanisms;
 
 import com.team766.framework.Mechanism;
+import com.team766.hal.EncoderReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.SpeedController;
+import com.team766.logging.Category;
 
 public class Drive extends Mechanism {
 	private SpeedController m_leftMotor;
 	private SpeedController m_rightMotor;
+	private EncoderReader m_leftEncoder;
+	private EncoderReader m_rightEncoder;
 
 	public Drive() {
+		loggerCategory = Category.DRIVE;
+
 		m_leftMotor = RobotProvider.instance.getMotor("drive.leftMotor");
 		m_rightMotor = RobotProvider.instance.getMotor("drive.rightMotor");
+		m_leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
+		m_rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
 	}
 
 	public void setDrivePower(double leftPower, double rightPower) {
@@ -24,5 +32,19 @@ public class Drive extends Mechanism {
 		double leftMotorPower = turn + forward;
 		double rightMotorPower = -turn + forward;
 		setDrivePower(leftMotorPower, rightMotorPower);
+	}
+
+	public double getEnoderDistance() {
+		double leftValue = m_leftEncoder.getDistance();
+		double rightValue = m_rightEncoder.getDistance();
+		log("Encoders: " + leftValue + " " + rightValue);
+		return (leftValue + rightValue) / 2;
+	}
+
+	public void resetEncoders() {
+		checkContextOwnership();
+
+		m_leftEncoder.reset();
+		m_rightEncoder.reset();
 	}
 }

src/main/java/com/team766/frc2020/procedures/DriveDistance.java
@@ -1,0 +1,21 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.frc2020.Robot;
+
+public class DriveDistance extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.resetEncoders();
+
+		Robot.drive.setDrivePower(0.25, 0.25);
+
+		context.waitFor(() -> Robot.drive.getEnoderDistance() >= 60.0);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}

Team766/2024

src/main/java/com/team766/robot/mechanisms/Drive.java
@@ -1,16 +1,24 @@
 package com.team766.robot.mechanisms;
 
 import com.team766.framework.Mechanism;
+import com.team766.hal.EncoderReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.MotorController;
+import com.team766.logging.Category;
 
 public class Drive extends Mechanism {
 	private MotorController leftMotor;
 	private MotorController rightMotor;
+	private EncoderReader leftEncoder;
+	private EncoderReader rightEncoder;
 
 	public Drive() {
+		loggerCategory = Category.DRIVE;
+
 		leftMotor = RobotProvider.instance.getMotor("drive.leftMotor");
 		rightMotor = RobotProvider.instance.getMotor("drive.rightMotor");
+		leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
+		rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
 	}
 
 	public void setDrivePower(double leftPower, double rightPower) {
@@ -24,5 +32,19 @@ public class Drive extends Mechanism {
 		double leftMotorPower = turn + forward;
 		double rightMotorPower = -turn + forward;
 		setDrivePower(leftMotorPower, rightMotorPower);
+	}
+
+	public double getEncoderDistance() {
+		double leftValue = leftEncoder.getDistance();
+		double rightValue = rightEncoder.getDistance();
+		log("Encoders: " + leftValue + " " + rightValue);
+		return (leftValue + rightValue) / 2;
+	}
+
+	public void resetEncoders() {
+		checkContextOwnership();
+
+		leftEncoder.reset();
+		rightEncoder.reset();
 	}
 }

src/main/java/com/team766/robot/procedures/DriveDistance.java
@@ -1,0 +1,21 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.robot.Robot;
+
+public class DriveDistance extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.resetEncoders();
+
+		Robot.drive.setDrivePower(0.25, 0.25);
+
+		context.waitFor(() -> Robot.drive.getEncoderDistance() >= 60.0);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}