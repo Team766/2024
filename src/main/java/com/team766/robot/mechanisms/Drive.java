stinkystinkman/MaroonFramework

Gyro and TurnAngle

src/main/java/com/team766/frc2020/mechanisms/Drive.java
@@ -2,6 +2,7 @@ package com.team766.frc2020.mechanisms;
 
 import com.team766.framework.Mechanism;
 import com.team766.hal.EncoderReader;
+import com.team766.hal.GyroReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.SpeedController;
 import com.team766.logging.Category;
@@ -11,6 +12,7 @@ public class Drive extends Mechanism {
 	private SpeedController m_rightMotor;
 	private EncoderReader m_leftEncoder;
 	private EncoderReader m_rightEncoder;
+	private GyroReader m_gyro;
 
 	public Drive() {
 		loggerCategory = Category.DRIVE;
@@ -19,6 +21,7 @@ public class Drive extends Mechanism {
 		m_rightMotor = RobotProvider.instance.getMotor("drive.rightMotor");
 		m_leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
 		m_rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
+		m_gyro = RobotProvider.instance.getGyro("drive.gyro");
 	}
 
 	public void setDrivePower(double leftPower, double rightPower) {
@@ -46,5 +49,17 @@ public class Drive extends Mechanism {
 
 		m_leftEncoder.reset();
 		m_rightEncoder.reset();
+	}
+
+	public void resetGyro() {
+		checkContextOwnership();
+
+		m_gyro.reset();
+	}
+
+	public double getGyroAngle() {
+		double angle = m_gyro.getAngle();
+		log("Gyro: " + angle);
+		return angle;
 	}
 }

src/main/java/com/team766/frc2020/procedures/TurnAngle.java
@@ -1,0 +1,24 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.frc2020.Robot;
+
+public class TurnAngle extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.resetGyro();
+
+		Robot.drive.setArcadeDrivePower(0.0, 0.25);
+		context.waitFor(() -> Robot.drive.getGyroAngle() <= -45);
+		Robot.drive.setArcadeDrivePower(0.0, 0.10);
+		context.waitFor(() -> Robot.drive.getGyroAngle() <= -80);
+		Robot.drive.setArcadeDrivePower(0.0, 0.01);
+		context.waitFor(() -> Robot.drive.getGyroAngle() <= -90);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}

Team766/2024

src/main/java/com/team766/robot/mechanisms/Drive.java
@@ -2,6 +2,7 @@ package com.team766.robot.mechanisms;
 
 import com.team766.framework.Mechanism;
 import com.team766.hal.EncoderReader;
+import com.team766.hal.GyroReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.MotorController;
 import com.team766.logging.Category;
@@ -11,6 +12,7 @@ public class Drive extends Mechanism {
 	private MotorController rightMotor;
 	private EncoderReader leftEncoder;
 	private EncoderReader rightEncoder;
+	private GyroReader gyro;
 
 	public Drive() {
 		loggerCategory = Category.DRIVE;
@@ -19,6 +21,7 @@ public class Drive extends Mechanism {
 		rightMotor = RobotProvider.instance.getMotor("drive.rightMotor");
 		leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
 		rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
+		gyro = RobotProvider.instance.getGyro("drive.gyro");
 	}
 
 	public void setDrivePower(double leftPower, double rightPower) {
@@ -46,5 +49,17 @@ public class Drive extends Mechanism {
 
 		leftEncoder.reset();
 		rightEncoder.reset();
+	}
+
+	public void resetGyro() {
+		checkContextOwnership();
+
+		gyro.reset();
+	}
+
+	public double getGyroAngle() {
+		double angle = gyro.getAngle();
+		log("Gyro: " + angle);
+		return angle;
 	}
 }

src/main/java/com/team766/robot/procedures/TurnAngle.java
@@ -1,0 +1,24 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.robot.Robot;
+
+public class TurnAngle extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.resetGyro();
+
+		Robot.drive.setArcadeDrivePower(0.0, 0.25);
+		context.waitFor(() -> Robot.drive.getGyroAngle() <= -45);
+		Robot.drive.setArcadeDrivePower(0.0, 0.10);
+		context.waitFor(() -> Robot.drive.getGyroAngle() <= -80);
+		Robot.drive.setArcadeDrivePower(0.0, 0.01);
+		context.waitFor(() -> Robot.drive.getGyroAngle() <= -90);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}