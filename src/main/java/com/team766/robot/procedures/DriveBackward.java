stinkystinkman/MaroonFramework

Score 5 balls project

src/main/java/com/team766/frc2020/procedures/DriveBackward.java
@@ -1,0 +1,19 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.frc2020.Robot;
+
+public class DriveBackward extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.setDrivePower(-0.25, -0.25);
+
+		context.waitForSeconds(2.7);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}

src/main/java/com/team766/frc2020/procedures/Score5.java
@@ -1,0 +1,27 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.frc2020.Robot;
+import com.team766.framework.Context;
+
+public class Score5 extends Procedure {
+
+	public void run(Context context) {
+		new CollectBalls().run(context);
+
+		new DriveBackward().run(context);
+
+		new TurnRight().run(context);
+
+		context.takeOwnership(Robot.drive);
+		Robot.drive.setDrivePower(0.25, 0.25);
+		context.waitForSeconds(0.8);
+		Robot.drive.setDrivePower(0.0, 0.0);
+		context.waitForSeconds(0.5);
+
+		for (int i = 0; i < 5; ++i) {
+			new Launch().run(context);
+		}
+	}
+
+}

Team766/2024

src/main/java/com/team766/robot/procedures/DriveBackward.java
@@ -1,0 +1,19 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.robot.Robot;
+
+public class DriveBackward extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.setDrivePower(-0.25, -0.25);
+
+		context.waitForSeconds(2.7);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}

src/main/java/com/team766/robot/procedures/Score5.java
@@ -1,0 +1,27 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.robot.Robot;
+import com.team766.framework.Context;
+
+public class Score5 extends Procedure {
+
+	public void run(Context context) {
+		new CollectBalls().run(context);
+
+		new DriveBackward().run(context);
+
+		new TurnRight().run(context);
+
+		context.takeOwnership(Robot.drive);
+		Robot.drive.setDrivePower(0.25, 0.25);
+		context.waitForSeconds(0.8);
+		Robot.drive.setDrivePower(0.0, 0.0);
+		context.waitForSeconds(0.5);
+
+		for (int i = 0; i < 5; ++i) {
+			new Launch().run(context);
+		}
+	}
+
+}