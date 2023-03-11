stinkystinkman/MaroonFramework

Split arcade drive tutorial code

src/main/java/com/team766/frc2020/OI.java
@@ -30,7 +30,7 @@ public class OI extends Procedure {
 		while (true) {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			Robot.drive.setArcadeDrivePower(m_joystick0.getAxis(1), m_joystick0.getAxis(0));
+			Robot.drive.setArcadeDrivePower(m_joystick0.getAxis(1), m_joystick1.getAxis(0));
 
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}

Team766/2024

src/main/java/com/team766/robot/OI.java
@@ -34,7 +34,7 @@ public class OI extends Procedure {
 
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			Robot.drive.setArcadeDrivePower(joystick0.getAxis(1), joystick0.getAxis(0));
+			Robot.drive.setArcadeDrivePower(joystick0.getAxis(1), joystick1.getAxis(0));
 		}
 	}
 }