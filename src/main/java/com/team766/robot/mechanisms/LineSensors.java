stinkystinkman/MaroonFramework

LineSensors and FollowLine

src/main/java/com/team766/frc2020/mechanisms/LineSensors.java
@@ -1,0 +1,29 @@
+package com.team766.frc2020.mechanisms;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.DigitalInputReader;
+import com.team766.hal.RobotProvider;
+
+public class LineSensors extends Mechanism {
+	private DigitalInputReader m_lineSensorLeft;
+	private DigitalInputReader m_lineSensorCenter;
+	private DigitalInputReader m_lineSensorRight;
+
+	public LineSensors() {
+		m_lineSensorLeft = RobotProvider.instance.getDigitalInput("lineSensorLeft");
+		m_lineSensorCenter = RobotProvider.instance.getDigitalInput("lineSensorCenter");
+		m_lineSensorRight = RobotProvider.instance.getDigitalInput("lineSensorRight");
+	}
+
+	public boolean getLineSensorLeft() {
+		return m_lineSensorLeft.get();
+	}
+
+	public boolean getLineSensorCenter() {
+		return m_lineSensorCenter.get();
+	}
+
+	public boolean getLineSensorRight() {
+		return m_lineSensorRight.get();
+	}
+}

src/main/java/com/team766/frc2020/procedures/FollowLine.java
@@ -1,0 +1,30 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.frc2020.Robot;
+
+public class FollowLine extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+		context.takeOwnership(Robot.lineSensors);
+
+		while (true) {
+			// Add line following code here
+			double steering = 0;
+			if (Robot.lineSensors.getLineSensorLeft()) {
+				steering = -0.45;
+			} else if (Robot.lineSensors.getLineSensorRight()) {
+				steering = 0.45;
+			}
+			if (Robot.lineSensors.getLineSensorCenter()) {
+				steering /= 4;
+			}
+			Robot.drive.setArcadeDrivePower(0.20, steering);
+
+			context.yield();
+		}
+	}
+
+}

Team766/2024

src/main/java/com/team766/robot/mechanisms/LineSensors.java
@@ -1,0 +1,29 @@
+package com.team766.robot.mechanisms;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.DigitalInputReader;
+import com.team766.hal.RobotProvider;
+
+public class LineSensors extends Mechanism {
+	private DigitalInputReader lineSensorLeft;
+	private DigitalInputReader lineSensorCenter;
+	private DigitalInputReader lineSensorRight;
+
+	public LineSensors() {
+		lineSensorLeft = RobotProvider.instance.getDigitalInput("lineSensorLeft");
+		lineSensorCenter = RobotProvider.instance.getDigitalInput("lineSensorCenter");
+		lineSensorRight = RobotProvider.instance.getDigitalInput("lineSensorRight");
+	}
+
+	public boolean getLineSensorLeft() {
+		return lineSensorLeft.get();
+	}
+
+	public boolean getLineSensorCenter() {
+		return lineSensorCenter.get();
+	}
+
+	public boolean getLineSensorRight() {
+		return lineSensorRight.get();
+	}
+}

src/main/java/com/team766/robot/procedures/FollowLine.java
@@ -1,0 +1,30 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.robot.Robot;
+
+public class FollowLine extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+		context.takeOwnership(Robot.lineSensors);
+
+		while (true) {
+			// Add line following code here
+			double steering = 0;
+			if (Robot.lineSensors.getLineSensorLeft()) {
+				steering = -0.45;
+			} else if (Robot.lineSensors.getLineSensorRight()) {
+				steering = 0.45;
+			}
+			if (Robot.lineSensors.getLineSensorCenter()) {
+				steering /= 4;
+			}
+			Robot.drive.setArcadeDrivePower(0.20, steering);
+
+			context.yield();
+		}
+	}
+
+}