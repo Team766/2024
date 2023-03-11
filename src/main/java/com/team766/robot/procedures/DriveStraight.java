stinkystinkman/MaroonFramework

Drive Straight tutorial code

src/main/java/com/team766/frc2020/procedures/DriveStraight.java
@@ -1,0 +1,19 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.frc2020.Robot;
+
+public class DriveStraight extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.setDrivePower(0.25, 0.25);
+
+		context.waitForSeconds(3.0);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}

Team766/2024

src/main/java/com/team766/robot/procedures/DriveStraight.java
@@ -1,0 +1,19 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.robot.Robot;
+
+public class DriveStraight extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
+		Robot.drive.setDrivePower(0.25, 0.25);
+
+		context.waitForSeconds(3.0);
+
+		Robot.drive.setDrivePower(0.0, 0.0);
+	}
+
+}