da61a8be32fcd9e7b72cfddbaa0a55d39245965e
Max <mspier00001@gmail.com>
2024-01-08 08:54:43 -0800

    some javadocs

--- src/main/java/com/team766/robot/mechanisms/SpeakerShooterPowerCalculator.java
+++ src/main/java/com/team766/robot/mechanisms/SpeakerShooterPowerCalculator.java
@@ -54,6 +54,15 @@ public class SpeakerShooterPowerCalculator extends Mechanism {
 
 	}
 
+	/*
+	 * This method will shoot the ball into the speaker.
+	 * It will move the robot to the closest scoring position, where it will then shoot.
+	 * This method is not complete, it is basically just psuedocode right now.
+	 * Once we have actual mechanism code (or at least a design), this can be finished.
+	 * 
+	 * @throws AprilTagGeneralCheckedException previous exceptions that could have arisen from any abstracted method calls.
+	 * @author Max Spier, 1/7/2024
+	 */
 	public void shoot() throws AprilTagGeneralCheckedException{
 		ScoringPosition score = closestTo();
 
@@ -80,6 +89,14 @@ public class SpeakerShooterPowerCalculator extends Mechanism {
 		
 	}
 
+	/*
+	 * This method will return the transform3d of the robot to the tag.
+	 * It checks to make sure the tag is of the correct tag ID (according to the current alliance), where it will then give that transform3d.
+	 * 
+	 * @return Transform3d of the robot to the tag
+	 * @throws AprilTagGeneralCheckedException if the tag is not found by any camera
+	 * @author Max Spier, 1/7/2024
+	 */
 	private Transform3d getTransform3dOfRobotToTag() throws AprilTagGeneralCheckedException{
 		CameraPlus toUse;
 		try{
@@ -93,6 +110,15 @@ public class SpeakerShooterPowerCalculator extends Mechanism {
 		return robotToTag;
 	}
 
+	/*
+	 * This method will return the closest scoring position to the robot.
+	 * It will use the robotToTag transform3d to find the closest position.
+	 * 
+	 * @return ScoringPosition of the closest position to the robot
+	 * @throws AprilTagGeneralCheckedException previous exceptions that could have arisen from any abstracted method calls.
+	 * @author Max Spier, 1/7/2024
+	 */
+	 
 	private ScoringPosition closestTo() throws AprilTagGeneralCheckedException{
 
 		Transform3d robotToTag = this.getTransform3dOfRobotToTag();
@@ -100,7 +126,7 @@ public class SpeakerShooterPowerCalculator extends Mechanism {
 		double x = robotToTag.getX();
 		double y = robotToTag.getY();
 
-		//destroy transform3d to save memory
+		//destroy transform3d to save memory?
 
 		double left = Math.sqrt(Math.pow(x - leftPosition.x_position,2) + Math.pow(y - leftPosition.y_position, 2));
 		double center = Math.sqrt(Math.pow(x - centerPosition.x_position,2) + Math.pow(y - centerPosition.y_position, 2));
