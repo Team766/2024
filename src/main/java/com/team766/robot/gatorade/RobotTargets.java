8473315569169a38b404f81016e025bbfee3465d
1yd1a <90945906+1yd1a@users.noreply.github.com>
2024-01-13 16:12:57 -0800

    replace point and pointdir

--- src/main/java/com/team766/robot/gatorade/RobotTargets.java
+++ src/main/java/com/team766/robot/gatorade/RobotTargets.java
@@ -1,9 +1,9 @@
 package com.team766.robot.gatorade;
 
-import com.team766.odometry.Point;
+import edu.wpi.first.math.geometry.Translation2d;
 
 /**
- * Constants defining points the robot may want to travel to
+ * Constants defining translation2ds the robot may want to travel to
  */
 public final class RobotTargets {
 
@@ -20,29 +20,29 @@ public final class RobotTargets {
     private static final double ROW_8 = 4.424426;
     private static final double ROW_9 = 4.9784;
 
-    // Points corresponding to nodes, in the format: NODES_COLOR_GRID_COLUMN
+    // Translation2ds corresponding to nodes, in the format: NODES_COLOR_GRID_COLUMN
     // This is from the perspective of the driver
-    public static final Point NODES_BLUE_RIGHT_RIGHT = new Point(BLUE_Y, ROW_1);
-    public static final Point NODES_BLUE_RIGHT_CENTER = new Point(BLUE_Y, ROW_2);
-    public static final Point NODES_BLUE_RIGHT_LEFT = new Point(BLUE_Y, ROW_3);
-    public static final Point NODES_BLUE_CENTER_RIGHT = new Point(BLUE_Y, ROW_4);
-    public static final Point NODES_BLUE_CENTER_CENTER = new Point(BLUE_Y, ROW_5);
-    public static final Point NODES_BLUE_CENTER_LEFT = new Point(BLUE_Y, ROW_6);
-    public static final Point NODES_BLUE_LEFT_RIGHT = new Point(BLUE_Y, ROW_7);
-    public static final Point NODES_BLUE_LEFT_CENTER = new Point(BLUE_Y, ROW_8);
-    public static final Point NODES_BLUE_LEFT_LEFT = new Point(BLUE_Y, ROW_9);
+    public static final Translation2d NODES_BLUE_RIGHT_RIGHT = new Translation2d(BLUE_Y, ROW_1);
+    public static final Translation2d NODES_BLUE_RIGHT_CENTER = new Translation2d(BLUE_Y, ROW_2);
+    public static final Translation2d NODES_BLUE_RIGHT_LEFT = new Translation2d(BLUE_Y, ROW_3);
+    public static final Translation2d NODES_BLUE_CENTER_RIGHT = new Translation2d(BLUE_Y, ROW_4);
+    public static final Translation2d NODES_BLUE_CENTER_CENTER = new Translation2d(BLUE_Y, ROW_5);
+    public static final Translation2d NODES_BLUE_CENTER_LEFT = new Translation2d(BLUE_Y, ROW_6);
+    public static final Translation2d NODES_BLUE_LEFT_RIGHT = new Translation2d(BLUE_Y, ROW_7);
+    public static final Translation2d NODES_BLUE_LEFT_CENTER = new Translation2d(BLUE_Y, ROW_8);
+    public static final Translation2d NODES_BLUE_LEFT_LEFT = new Translation2d(BLUE_Y, ROW_9);
 
-    public static final Point NODES_RED_LEFT_LEFT = new Point(RED_Y, ROW_1);
-    public static final Point NODES_RED_LEFT_CENTER = new Point(RED_Y, ROW_2);
-    public static final Point NODES_RED_LEFT_RIGHT = new Point(RED_Y, ROW_3);
-    public static final Point NODES_RED_CENTER_LEFT = new Point(RED_Y, ROW_4);
-    public static final Point NODES_RED_CENTER_CENTER = new Point(RED_Y, ROW_5);
-    public static final Point NODES_RED_CENTER_RIGHT = new Point(RED_Y, ROW_6);
-    public static final Point NODES_RED_RIGHT_LEFT = new Point(RED_Y, ROW_7);
-    public static final Point NODES_RED_RIGHT_CENTER = new Point(RED_Y, ROW_8);
-    public static final Point NODES_RED_RIGHT_RIGHT = new Point(RED_Y, ROW_9);
+    public static final Translation2d NODES_RED_LEFT_LEFT = new Translation2d(RED_Y, ROW_1);
+    public static final Translation2d NODES_RED_LEFT_CENTER = new Translation2d(RED_Y, ROW_2);
+    public static final Translation2d NODES_RED_LEFT_RIGHT = new Translation2d(RED_Y, ROW_3);
+    public static final Translation2d NODES_RED_CENTER_LEFT = new Translation2d(RED_Y, ROW_4);
+    public static final Translation2d NODES_RED_CENTER_CENTER = new Translation2d(RED_Y, ROW_5);
+    public static final Translation2d NODES_RED_CENTER_RIGHT = new Translation2d(RED_Y, ROW_6);
+    public static final Translation2d NODES_RED_RIGHT_LEFT = new Translation2d(RED_Y, ROW_7);
+    public static final Translation2d NODES_RED_RIGHT_CENTER = new Translation2d(RED_Y, ROW_8);
+    public static final Translation2d NODES_RED_RIGHT_RIGHT = new Translation2d(RED_Y, ROW_9);
 
-    public static final Point[] NODES = {
+    public static final Translation2d[] NODES = {
         NODES_BLUE_RIGHT_RIGHT,
         NODES_BLUE_RIGHT_CENTER,
         NODES_BLUE_RIGHT_LEFT,
@@ -63,7 +63,7 @@ public final class RobotTargets {
         NODES_RED_LEFT_LEFT
     };
 
-    public static final Point[] CONE_NODES = {
+    public static final Translation2d[] CONE_NODES = {
         NODES_BLUE_RIGHT_RIGHT,
         NODES_BLUE_RIGHT_LEFT,
         NODES_BLUE_CENTER_RIGHT,
@@ -78,7 +78,7 @@ public final class RobotTargets {
         NODES_RED_LEFT_LEFT
     };
 
-    public static final Point[] CUBE_NODES = {
+    public static final Translation2d[] CUBE_NODES = {
         NODES_BLUE_RIGHT_CENTER,
         NODES_BLUE_CENTER_CENTER,
         NODES_BLUE_LEFT_CENTER,
