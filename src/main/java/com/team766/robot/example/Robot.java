4fcb1b1e3a910be7d83d7e650d7df737cafff74c
Debajit Ghosh <dg@dejabotz.com>
2024-01-15 22:02:24 -0800

    implementing RobotConfigurator in various Robot classes

--- src/main/java/com/team766/robot/example/Robot.java
+++ src/main/java/com/team766/robot/example/Robot.java
@@ -1,11 +1,28 @@
 package com.team766.robot.example;
 
+import com.team766.framework.AutonomousMode;
+import com.team766.framework.Procedure;
+import com.team766.hal.RobotConfigurator;
 import com.team766.robot.example.mechanisms.*;
 
-public class Robot {
-    // Declare mechanisms here
+public class Robot implements RobotConfigurator {
+    // Declare mechanisms (as static fields) here
+
+    public Robot() {}
 
     public static void robotInit() {
         // Initialize mechanisms here
+    }
+
+    public void initializeMechanisms() {
+        robotInit();
+    }
+
+    public Procedure createOI() {
+        return new OI();
+    }
+
+    public AutonomousMode[] getAutonomousModes() {
+        return AutonomousModes.AUTONOMOUS_MODES;
     }
 }
--- src/main/java/com/team766/robot/gatorade/Robot.java
+++ src/main/java/com/team766/robot/gatorade/Robot.java
@@ -1,16 +1,21 @@
 package com.team766.robot.gatorade;
 
+import com.team766.framework.AutonomousMode;
+import com.team766.framework.Procedure;
+import com.team766.hal.RobotConfigurator;
 import com.team766.robot.common.mechanisms.Drive;
 import com.team766.robot.gatorade.mechanisms.*;
 
-public class Robot {
-    // Declare mechanisms here
+public class Robot implements RobotConfigurator {
+    // Declare mechanisms (as static fields) here
     public static Intake intake;
     public static Wrist wrist;
     public static Elevator elevator;
     public static Shoulder shoulder;
     public static Drive drive;
     public static Lights lights;
+
+    public Robot() {}
 
     public static void robotInit() {
         // Initialize mechanisms here
@@ -20,5 +25,17 @@ public class Robot {
         shoulder = new Shoulder();
         drive = new Drive();
         lights = new Lights();
+    }
+
+    public void initializeMechanisms() {
+        robotInit();
+    }
+
+    public Procedure createOI() {
+        return new OI();
+    }
+
+    public AutonomousMode[] getAutonomousModes() {
+        return AutonomousModes.AUTONOMOUS_MODES;
     }
 }
