6a926b2d3af5180839112722fde8a4aba87f5b8f
Max <68516760+TTVMixmix00@users.noreply.github.com>
2024-01-19 19:56:06 -0800

    checkpoint

--- src/main/java/com/team766/ViSIONbase/StaticCameras.java
+++ src/main/java/com/team766/ViSIONbase/StaticCameras.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 public class StaticCameras {
 
 	public static final CameraPlus camera2 = new CameraPlus("Vision Camera 2 1690");
-	public static ArrayList<CameraPlus> cameras = new ArrayList<CameraPlus>(){
+	public static ArrayList<CameraPlus> apriltagCameras = new ArrayList<CameraPlus>(){
 		{
 			add(camera2);
 		}
 	};
+
+	public static final CameraPlus noteDetectorCamera = new CameraPlus("Note Detection Camera");
+	
+
 }
