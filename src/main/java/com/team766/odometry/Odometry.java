02ee7586d774783e6429d0bc9ea2802860ac34b2
1yd1a <90945906+1yd1a@users.noreply.github.com>
2024-01-19 21:16:27 -0800

    LINT!

--- src/main/java/com/team766/odometry/Odometry.java
+++ src/main/java/com/team766/odometry/Odometry.java
@@ -1,19 +1,18 @@
 package com.team766.odometry;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.team766.hal.GyroReader;
 import com.team766.hal.MotorController;
 import com.team766.library.RateLimiter;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.team766.framework.LoggingBase;
 import com.team766.logging.Category;
 import com.team766.logging.Logger;
 import com.team766.logging.Severity;
 import com.team766.robot.gatorade.*;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import java.util.Optional;
@@ -167,9 +166,7 @@ public class Odometry {
             currPositions[i] =
                     new Pose2d(
                             currPositions[i].getTranslation(),
-                            gyroPosition.plus(
-                                    Rotation2d.fromDegrees(
-                                            -absolutePosition)));
+                            gyroPosition.plus(Rotation2d.fromDegrees(-absolutePosition)));
 
             rotationChange = currPositions[i].getRotation().minus(prevPositions[i].getRotation());
 
