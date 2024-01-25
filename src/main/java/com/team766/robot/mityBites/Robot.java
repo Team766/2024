80502ddcaeb724343af67cd595a4de83cc31e311
Leek865 <thatpeacockoverthere@gmail.com>
2024-01-24 19:36:35 -0800

    created more permanent branch
    doesnt build yet

---
+++ src/main/java/com/team766/robot/mityBites/Robot.java
@@ -0,0 +1,25 @@
+package com.team766.robot.mityBites;
+
+import com.team766.framework.AutonomousMode;
+import com.team766.framework.Procedure;
+import com.team766.hal.RobotConfigurator;
+import com.team766.robot.example.mechanisms.*;
+
+public class Robot implements RobotConfigurator {
+    // Declare mechanisms (as static fields) here
+
+    @Override
+    public void initializeMechanisms() {
+        // Initialize mechanisms here
+    }
+
+    @Override
+    public Procedure createOI() {
+        return new OI();
+    }
+
+    @Override
+    public AutonomousMode[] getAutonomousModes() {
+        return AutonomousModes.AUTONOMOUS_MODES;
+    }
+}
