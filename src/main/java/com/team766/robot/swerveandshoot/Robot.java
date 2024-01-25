a2a08f50b528ae6784418a0d8c9bae750057b9c5
Max <68516760+TTVMixmix00@users.noreply.github.com>
2024-01-24 18:30:31 -0800

    updating Robot to latest code

--- src/main/java/com/team766/robot/swerveandshoot/Robot.java
+++ src/main/java/com/team766/robot/swerveandshoot/Robot.java
@@ -1,18 +1,35 @@
 package com.team766.robot.swerveandshoot;
 
+import com.team766.framework.AutonomousMode;
+import com.team766.framework.Procedure;
+import com.team766.hal.RobotConfigurator;
+import com.team766.robot.common.mechanisms.Drive;
 import com.team766.robot.swerveandshoot.mechanisms.*;
 
-public class Robot {
+public class Robot implements RobotConfigurator {
     // Declare mechanisms here
     public static TempPickerUpper tempPickerUpper;
     public static TempShooter tempShooter;
     public static NoteUtil noteUtil;
 	public static Lights lights;
+    public static Drive drive;
 
-    public static void robotInit() {
+    @Override
+    public void initializeMechanisms() {
         tempPickerUpper = new TempPickerUpper();
         tempShooter = new TempShooter();
         noteUtil = new NoteUtil();
 		lights = new Lights();
+        drive = new Drive();
+    }
+
+    @Override
+    public Procedure createOI() {
+        return new OI();
+    }
+
+    @Override
+    public AutonomousMode[] getAutonomousModes() {
+        return AutonomousModes.AUTONOMOUS_MODES;
     }
 }
