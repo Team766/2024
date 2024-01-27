f82edd126dd0184778a71fa25934fcb65f3fa288
Max <68516760+TTVMixmix00@users.noreply.github.com>
2024-01-26 19:16:28 -0800

    first pass implement drive to note

--- src/main/java/com/team766/robot/swerveandshoot/mechanisms/NoteUtil.java
+++ src/main/java/com/team766/robot/swerveandshoot/mechanisms/NoteUtil.java
@@ -1,12 +1,20 @@
 package com.team766.robot.swerveandshoot.mechanisms;
 
 import com.team766.ViSIONbase.*;
+import com.team766.controllers.PIDController;
 import com.team766.framework.AprilTagGeneralCheckedException;
 import com.team766.framework.Mechanism;
+import com.team766.robot.swerveandshoot.Robot;
 
 public class NoteUtil extends Mechanism {
 
-    public NoteUtil() {}
+    private PIDController yawPID;
+    
+    public NoteUtil() {
+        //set reasonable deadzone!
+        yawPID = new PIDController(null, null, null, null, null, null, null);
+        yawPID.setSetpoint(0.00);
+    }
 
     public String toString() {
         try {
@@ -31,25 +39,26 @@ public class NoteUtil extends Mechanism {
     }
 
     public void goToNote() throws AprilTagGeneralCheckedException {
-        if (getStatus() == 1) {
+        
+        if (!hasNoteInIntake()) {
 
             double yawInDegrees = StaticCameras.noteDetectorCamera.getYawOfRing();
+            yawPID.calculate(yawInDegrees);
+            double power = yawPID.getOutput();
 
-            double pitchInDegrees = StaticCameras.noteDetectorCamera.getPitchOfRing();
-
-            if (Math.abs(yawInDegrees) < 0.5) {
-
-                // Drive straight at the ring
-                // Do a solid power, increment down?
-                // Wait until ring is detected in intake
-
-            } else {
-                if (yawInDegrees < 0) {
-                    // drive right
-                } else {
-                    // drive left
-                }
+            if(Math.abs(power) > 0){
+                // check inverted power, check interchanged x and y
+                Robot.drive.controlRobotOriented(power, 0, 0);
+            }else{
+                Robot.drive.controlRobotOriented(0, 0.2,0);
             }
+
+            //double pitchInDegrees = StaticCameras.noteDetectorCamera.getPitchOfRing();
+
         }
+    }
+
+    public boolean hasNoteInIntake(){
+        return false;
     }
 }
