aad9c01b4471e0510f57afc6f7ccd88e12f78ec7
TheLegendOfKitty <61601745+TheLegendOfKitty@users.noreply.github.com>
2024-01-29 19:17:48 -0800

    followpath class

---
+++ src/main/java/com/team766/robot/gatorade/procedures/FollowPath.java
@@ -0,0 +1,15 @@
+package com.team766.robot.gatorade.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+
+public class FollowPath extends Procedure {
+    public FollowPath() {
+
+    }
+
+    @Override
+    public void run(Context context) {
+
+    }
+}
