c77a1744c9d040a96764d1ccd4487455776e0a38
dejabot <104333734+dejabot@users.noreply.github.com>
2024-02-10 12:29:17 -0800

    add support for reading, setting PID from motor config (#33)
    * add support for reading, setting PID from motor config
    
    * addressing PR comments

--- src/main/java/com/team766/hal/RobotProvider.java
+++ src/main/java/com/team766/hal/RobotProvider.java
@@ -146,7 +146,6 @@ public abstract class RobotProvider {
             }
             if (sensorInvertedConfig.valueOr(false)) {
                 motor.setSensorInverted(true);
-            }
             // check for, apply any PID settings that are in a sub-config
             configurePID(configName + ".pid.", motor);
 
