75d959c0f23b2f080161c3474f144e1ad8b44441
Debajit Ghosh <dg@dejabotz.com>
2024-02-17 13:16:50 -0800

    remove driver oi constants we moved

--- src/main/java/com/team766/robot/reva/constants/InputConstants.java
+++ src/main/java/com/team766/robot/reva/constants/InputConstants.java
@@ -9,15 +9,4 @@ public final class InputConstants {
     public static final int LEFT_JOYSTICK = 0;
     public static final int RIGHT_JOYSTICK = 1;
     public static final int BOXOP_GAMEPAD = 2; // should be in Logitech Mode
-
-    // Navigation
-    public static final int AXIS_LEFT_RIGHT = 0;
-    public static final int AXIS_FORWARD_BACKWARD = 1;
-    public static final int AXIS_TWIST = 3;
-
-    // Joystick buttons
-    public static final int FINE_DRIVING = 1;
-    public static final int CROSS_WHEELS = 3;
-    public static final int RESET_GYRO = 9;
-    public static final int RESET_POS = 15;
 }
