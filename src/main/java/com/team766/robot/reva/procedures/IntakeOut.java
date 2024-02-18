c70f69130e8faf1581ae7477b87e122a0572cb1f
Jonathan Gassend <115192551+The-Arx@users.noreply.github.com>
2024-02-17 22:29:39 -0800

    added intake code

--- src/main/java/com/team766/robot/reva/procedures/IntakeOut.java
+++ src/main/java/com/team766/robot/reva/procedures/IntakeOut.java
@@ -5,8 +5,9 @@ import com.team766.framework.Procedure;
 import com.team766.robot.reva.Robot;
 
 public class IntakeOut extends Procedure {
-    public void run(Context context) {
-        context.takeOwnership(Robot.intake);
-        Robot.intake.intakeOut();
-    }
+	public void run(Context context){
+		context.takeOwnership(Robot.intake);
+		Robot.intake.intakeOut();
+	}
+
 }
