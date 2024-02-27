5580d9923ca0ef3b20fc9094152b234fbdff0fef
Mixmix00 <mspier00001@gmail.com>
2024-02-27 08:26:58 -0800

    create new camera and scoring positions for stage

--- src/main/java/com/team766/robot/reva/VisionUtil/ScoringPositions.java
+++ src/main/java/com/team766/robot/reva/VisionUtil/ScoringPositions.java
@@ -5,4 +5,7 @@ import com.team766.ViSIONbase.ScoringPosition;
 public class ScoringPositions {
     public static final ScoringPosition MAKER_SPACE_1L = new ScoringPosition(0, 0, 2.88, -0.92, 0);
     public static final ScoringPosition MAKER_SPACE_1R = new ScoringPosition(0, 0, 2.9, 0.80, 0);
+
+    public static final ScoringPosition MAKER_SPACE_1L_STAGE = new ScoringPosition(null, null, null, null, null);
+    public static final ScoringPosition MAKER_SPACE_1R_STAGE = new ScoringPosition(null, null, null, null, null);
 }
