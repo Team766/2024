29058d80771fa0cff84d64dacfcdb942f0df863d
Max <68516760+Mixmix00@users.noreply.github.com>
2024-03-02 01:25:09 -0800

    final 2

--- src/main/java/com/team766/robot/reva/mechanisms/RightProximitySensor.java
+++ src/main/java/com/team766/robot/reva/mechanisms/RightProximitySensor.java
@@ -1,25 +1,27 @@
 package com.team766.robot.reva.mechanisms;
 
 import com.playingwithfusion.TimeOfFlight.*;
+import com.team766.config.ConfigFileReader;
 import com.team766.framework.Mechanism;
 
 public class RightProximitySensor extends Mechanism {
-	
-	private TimeOfFlight sensor;
 
+    private TimeOfFlight sensor;
 
-	//This should be the amount that getRange() should return less than for a note to be classified as in
-	private static double threshold = 0; // needs calibration
+    // This should be the amount that getRange() should return less than for a note to be classified
+    // as in
+    private static double threshold =
+            ConfigFileReader.getInstance()
+                    .getDouble("RightProximitySensor.threshold")
+                    .get(); // needs calibration
 
-	public RightProximitySensor(){
-		sensor = new TimeOfFlight(0); // needs calibration
+    public RightProximitySensor() {
+        sensor = new TimeOfFlight(0); // needs calibration
 
-		sensor.setRangingMode(RangingMode.Short);
-	}
+        sensor.setRangingMode(RangingMode.Short);
+    }
 
-	public boolean isNoteReady(){
-		return (threshold) > sensor.getRange() && sensor.isRangeValid();
-	}
-
-	
+    public boolean isNoteReady() {
+        return (threshold) > sensor.getRange() && sensor.isRangeValid();
+    }
 }
