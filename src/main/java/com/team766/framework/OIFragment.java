063fe0eae1bb99357d5093d0da7888a256089975
Debajit Ghosh <dg@dejabotz.com>
2024-03-09 10:20:58 -0800

    run the then behavior whenever the condition is triggered (not just on initial trigger).
    add javadoc.

--- src/main/java/com/team766/framework/OIFragment.java
+++ src/main/java/com/team766/framework/OIFragment.java
@@ -32,31 +32,58 @@ public class OIFragment extends LoggingBase {
         return name;
     }
 
+    /**
+     * Adds a rule for this OI.
+     * As long as the supplied condition is triggered, run the "then" behavior.
+     * If the condition gets triggered, once it stops being triggered, run the "whenDone" behavior.
+     *
+     * @param condition The condition to check.
+     * @param then The behavior to run when the condition is triggered.  May be null.
+     * @param whenDone The behavior to run after the condition had been triggered and is no longer triggered.  May be null.
+     */
     protected void addRule(
             BooleanSupplier condition, Consumer<Context> then, Consumer<Context> whenDone) {
         rules.add(new Rule(condition, then, whenDone));
     }
 
+    /**
+     * Any logic or computations to run before checking any rules.
+     *
+     * @param context The context being used to run this OIFragment.
+     */
     protected void pre(Context context) {}
 
+    /**
+     * Any logic or computations to run after checking all of the rules.
+     *
+     * @param context The context being used to run this OIFragment.
+     */
     protected void post(Context context) {}
 
     public void handleOI(Context context) {
+        // run any "pre" logic
         pre(context);
         for (Rule rule : rules) {
-            if (rule.triggered) {
-                if (!rule.condition.getAsBoolean()) {
-                    if (rule.whenDone != null) {
-                        rule.whenDone.accept(context);
-                    }
-                }
-            } else if (rule.condition.getAsBoolean()) {
+            // if this rule's condition is true, do the associated "then" action
+            if (rule.condition.getAsBoolean()) {
                 if (rule.then != null) {
                     rule.then.accept(context);
                 }
+                // and mark the rule as "triggered"
                 rule.triggered = true;
+            } else {
+                // the rule condition's is false
+                // see if the rule had been "triggered".  if so, do the associated "done" action.
+                if (rule.triggered) {
+                    if (rule.whenDone != null) {
+                        rule.whenDone.accept(context);
+                    }
+                    // and mark the rule as no longer being "triggered"
+                    rule.triggered = false;
+                }
             }
         }
+        // run any "post" logic
         post(context);
     }
 }
