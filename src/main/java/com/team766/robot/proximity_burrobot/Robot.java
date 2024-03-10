ea8132f8fb27e9c2476e3de2a422165168ea3c4a
Max <68516760+Mixmix00@users.noreply.github.com>
2024-03-09 19:13:17 -0800

    deleted prox sensor mech in test bot

--- src/main/java/com/team766/robot/proximity_burrobot/Robot.java
+++ src/main/java/com/team766/robot/proximity_burrobot/Robot.java
@@ -4,16 +4,13 @@ import com.team766.framework.AutonomousMode;
 import com.team766.framework.Procedure;
 import com.team766.hal.RobotConfigurator;
 import com.team766.robot.proximity_burrobot.mechanisms.*;
-import com.team766.robot.reva.mechanisms.RightProximitySensor;
 
 public class Robot implements RobotConfigurator {
     // Declare mechanisms (as static fields) here
-    public static RightProximitySensor proximitySensor;
 
     @Override
     public void initializeMechanisms() {
         // Initialize mechanisms here
-        proximitySensor = new RightProximitySensor();
     }
 
     @Override
