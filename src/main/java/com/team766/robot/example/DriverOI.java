56c761879d9a0318568ec4bd30eae26f55207884
Debajit Ghosh <dg@dejabotz.com>
2024-03-10 21:31:03 -0700

    addressing PR comments

--- src/main/java/com/team766/robot/example/DriverOI.java
+++ src/main/java/com/team766/robot/example/DriverOI.java
@@ -3,31 +3,40 @@ package com.team766.robot.example;
 import com.team766.framework.Context;
 import com.team766.framework.OIFragment;
 import com.team766.hal.JoystickReader;
+import com.team766.robot.example.constants.InputConstants;
 import com.team766.robot.example.procedures.*;
 
 public class DriverOI extends OIFragment {
-    private final JoystickReader leftJoystick;
+    private final JoystickReader joystick;
+    private double joystickX;
+    private double joystickY;
 
     // add mechanisms here
 
     // add any conditions (joystick inputs, etc)
-    Condition button0;
+    Condition button1;
     Condition moveJoystick;
 
     // add any mechanisms to the constructor arguments as well
-    public DriverOI(JoystickReader leftJoystick) {
+    public DriverOI(JoystickReader joystick) {
         super("DriverOI");
-        this.leftJoystick = leftJoystick;
+        this.joystick = joystick;
 
-        button0 = new Condition(() -> leftJoystick.getButton(0));
-        moveJoystick = new Condition(() -> leftJoystick.getAxis(0) > 0);
+        button1 = new Condition(() -> joystick.getButton(InputConstants.BUTTON_TRIGGER));
+        moveJoystick = new Condition(() -> Math.abs(joystickX) > 0 || Math.abs(joystickY) > 0);
+    }
+
+    @Override
+    protected void handlePre() {
+        joystickX = joystick.getAxis(InputConstants.AXIS_X);
+        joystickY = joystick.getAxis(InputConstants.AXIS_Y);
     }
 
     @Override
     protected void handleOI(Context context) {
-        if (button0.isNewlyTriggering()) {
+        if (button1.isNewlyTriggering()) {
             // handle button press
-        } else if (button0.isFinishedTriggering()) {
+        } else if (button1.isFinishedTriggering()) {
             // handle button release
         }
 
