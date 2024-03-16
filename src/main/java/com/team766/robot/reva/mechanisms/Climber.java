4e0cf8cb8f96fd8d6990328ac0bec5222a44f745
Jonathan Gassend <115192551+The-Arx@users.noreply.github.com>
2024-03-16 16:59:50 -0700

    Updated Climber.java according to feedback
    Removed PID methods , added separate left and right power, removed nudge and changed debug logs.

--- src/main/java/com/team766/robot/reva/mechanisms/Climber.java
+++ src/main/java/com/team766/robot/reva/mechanisms/Climber.java
@@ -30,18 +30,13 @@ public class Climber extends Mechanism {
     private MotorController leftMotor;
     private MotorController rightMotor;
 
-    // Are these only for dubugging?
-    private double leftTargetRotations = 0;
-    private double rightTargetRotations = 0;
-
     private static final double GEAR_RATIO_AND_CIRCUMFERENCE =
             (14. / 50.) * (30. / 42.) * (1.25 * Math.PI);
-    private static final double NUDGE_INCREMENT = 10; // in cm
-    private static final double PIDLESS_NUDGE_INCREMENT = 0.2;
-    private static final double SUPPLY_CURRENT_LIMIT = 30.0; // max efficiency from spec sheet
-    private static final double STATOR_CURRENT_LIMIT = 80.0; // TUNE THIS!
+    private static final double SUPPLY_CURRENT_LIMIT = 30; // max efficiency from spec sheet
+    private static final double STATOR_CURRENT_LIMIT = 80; // TUNE THIS!
 
-    private double pidlessPower = 0.0;
+    private double leftPower = 0;
+    private double rightPower = 0;
 
     public Climber() {
         leftMotor = RobotProvider.instance.getMotor(CLIMBER_LEFT_MOTOR);
@@ -55,81 +50,42 @@ public class Climber extends Mechanism {
         MotorUtil.setTalonFXStatorCurrentLimit(rightMotor, STATOR_CURRENT_LIMIT);
     }
 
-    public boolean isRunningNoPID() {
-        return pidlessPower != 0.0;
+    public void setPower(double power) {
+        setLeftPower(power);
+        setRightPower(power);
     }
 
-    public void goNoPIDUp() {
-        goNoPIDUpLeft();
-        goNoPIDUpRight();
+    public void setLeftPower(double power) {
+        leftMotor.set(power);
+        leftPower = power;
     }
 
-    public void goNoPIDUpLeft() {
-        leftMotor.set(PIDLESS_NUDGE_INCREMENT);
-    }
-
-    public void goNoPIDUpRight() {
-        rightMotor.set(PIDLESS_NUDGE_INCREMENT);
-    }
-
-    public void goNoPIDDown() {
-        goNoPIDDownLeft();
-        goNoPIDDownRight();
-    }
-
-    public void goNoPIDDownLeft() {
-        leftMotor.set(-PIDLESS_NUDGE_INCREMENT);
-    }
-
-    public void goNoPIDDownRight() {
-        rightMotor.set(-PIDLESS_NUDGE_INCREMENT);
+    public void setRightPower(double power) {
+        rightMotor.set(power);
+        rightPower = power;
     }
 
     public void stop() {
-        pidlessPower = 0.0;
-        leftMotor.stopMotor();
-        rightMotor.stopMotor();
+        stopLeft();
+        stopRight();
     }
 
-    private double heightToRotations(double height) {
+    public void stopLeft() {
+        leftMotor.stopMotor();
+        leftPower = 0;
+    }
+
+    public void stopRight() {
+        rightMotor.stopMotor();
+        rightPower = 0;
+    }
+
+    private static double heightToRotations(double height) {
         return height * GEAR_RATIO_AND_CIRCUMFERENCE;
     }
 
-    private double rotationsToHeight(double rotations) {
+    private static double rotationsToHeight(double rotations) {
         return rotations / GEAR_RATIO_AND_CIRCUMFERENCE;
-    }
-
-    public void setHeight(ClimberPosition position) {
-        setHeight(position.getHeight());
-    }
-
-    public void setHeight(double height) {
-        setHeightLeft(height);
-        setHeightRight(height);
-    }
-
-    private double setHeight(MotorController motor, double height) {
-        double targetHeight =
-                com.team766.math.Math.clamp(
-                        height,
-                        ClimberPosition.BOTTOM.getHeight(),
-                        ClimberPosition.TOP.getHeight());
-        double targetRotations = heightToRotations(targetHeight);
-        motor.set(MotorController.ControlMode.Position, targetRotations);
-        return targetRotations;
-    }
-
-    public void setHeightLeft(double height) {
-        leftTargetRotations = setHeight(leftMotor, height);
-    }
-
-    public void setHeightRight(double height) {
-        rightTargetRotations = setHeight(rightMotor, height);
-    }
-
-    // TODO: remove
-    public double getHeight() {
-        return getHeightLeft();
     }
 
     public double getHeightLeft() {
@@ -140,23 +96,14 @@ public class Climber extends Mechanism {
         return rotationsToHeight(rightMotor.getSensorPosition());
     }
 
-    public void nudgeUp() {
-        pidlessPower = Math.min(1, pidlessPower + PIDLESS_NUDGE_INCREMENT);
-        // setHeight(getHeight() + NUDGE_AMOUNT);
-    }
-
-    public void nudgeDown() {
-        pidlessPower = Math.max(-1, pidlessPower - PIDLESS_NUDGE_INCREMENT);
-        // setHeight(getHeight() - NUDGE_AMOUNT);
-    }
-
     @Override
     public void run() {
         SmartDashboard.putNumber("[CLIMBER] Left Rotations", leftMotor.getSensorPosition());
         SmartDashboard.putNumber("[CLIMBER] Right Rotations", rightMotor.getSensorPosition());
-        SmartDashboard.putNumber("[CLIMBER] Left Target Rotations", leftTargetRotations);
-        SmartDashboard.putNumber("[CLIMBER] Right Target Rotations", rightTargetRotations);
-        SmartDashboard.putNumber("[CLIMBER] Height", getHeight());
+        SmartDashboard.putNumber("[CLIMBER] Left Height", getHeightLeft());
+        SmartDashboard.putNumber("[CLIMBER] Right Height", getHeightRight());
+        SmartDashboard.putNumber("[CLIMBER] Left Power", leftPower);
+        SmartDashboard.putNumber("[CLIMBER] Right Power", rightPower);
         SmartDashboard.putNumber(
                 "[CLIMBER] Left Motor Supply Current", MotorUtil.getCurrentUsage(leftMotor));
         SmartDashboard.putNumber(
