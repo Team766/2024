27c3529ea6472bf8b5291049752a6760ee635451
Debajit Ghosh <dg@dejabotz.com>
2024-03-23 08:25:53 -0700

    linting

--- src/main/java/com/team766/robot/reva/AutonomousModes.java
+++ src/main/java/com/team766/robot/reva/AutonomousModes.java
@@ -1,10 +1,10 @@
 package com.team766.robot.reva;
 
 import com.team766.framework.AutonomousMode;
-import com.team766.robot.reva.procedures.ThreePieceAutonAmpSide;
 import com.team766.robot.reva.procedures.MoveClimbersToBottom;
 import com.team766.robot.reva.procedures.RotateAndShootNow;
 import com.team766.robot.reva.procedures.ShootOnePiece;
+import com.team766.robot.reva.procedures.ThreePieceAutonAmpSide;
 
 public class AutonomousModes {
     public static final AutonomousMode[] AUTONOMOUS_MODES =
