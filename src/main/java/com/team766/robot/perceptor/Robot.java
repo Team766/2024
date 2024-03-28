ed27d61c1970be6044d5dde6c5c9e5ba4dcaaec8
Debajit Ghosh <dg@dejabotz.com>
2024-03-27 19:00:34 -0700

    initial bringup for perceptor

---
+++ src/main/java/com/team766/robot/perceptor/Robot.java
@@ -0,0 +1,24 @@
+package com.team766.robot.perceptor;
+
+import com.team766.framework.AutonomousMode;
+import com.team766.framework.Procedure;
+import com.team766.hal.RobotConfigurator;
+
+public class Robot implements RobotConfigurator {
+    // Declare mechanisms (as static fields) here
+
+    @Override
+    public void initializeMechanisms() {
+        // Initialize mechanisms here
+    }
+
+    @Override
+    public Procedure createOI() {
+        return new OI();
+    }
+
+    @Override
+    public AutonomousMode[] getAutonomousModes() {
+        return AutonomousModes.AUTONOMOUS_MODES;
+    }
+}
---
+++ src/main/java/com/team766/robot/perceptor/procedures/DoNothing.java
@@ -0,0 +1,9 @@
+package com.team766.robot.perceptor.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+
+public class DoNothing extends Procedure {
+
+    public void run(final Context context) {}
+}
