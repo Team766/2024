8cdc43d3c49b944214cda3d69e99f01df7b93573
Mixmix00 <mspier00001@gmail.com>
2024-03-28 13:57:19 -0700

    some more

--- src/main/java/com/team766/robot/reva/VisionUtil/ScoringPositions.java
+++ src/main/java/com/team766/robot/reva/VisionUtil/ScoringPositions.java
@@ -1,5 +1,6 @@
 package com.team766.robot.reva.VisionUtil;
 
+import java.util.ArrayList;
 import com.team766.ViSIONbase.ScoringPosition;
 
 public class ScoringPositions {
@@ -10,5 +11,16 @@ public class ScoringPositions {
     public static final ScoringPosition BLUE_ALLIANCE_CENTERSTAGE = new ScoringPosition(0, 0, 0, 0, 0, 14);
     public static final ScoringPosition BLUE_ALLIANCE_STAGE_LEFT = new ScoringPosition(0, 0, 0, 0, 0, 15);
     public static final ScoringPosition BLUE_ALLIANCE_STAGE_RIGHT = new ScoringPosition(0, 0, 0, 0, 0, 16);
+
+    public static final ArrayList<ScoringPosition> trapScoringPositions = new ArrayList<ScoringPosition>(){
+        {
+            add(RED_ALLIANCE_STAGE_LEFT);
+            add(RED_ALLIANCE_STAGE_RIGHT);
+            add(RED_ALLIANCE_CENTERSTAGE);
+            add(BLUE_ALLIANCE_CENTERSTAGE);
+            add(BLUE_ALLIANCE_STAGE_LEFT);
+            add(BLUE_ALLIANCE_STAGE_RIGHT);
+        }
+    };
 }
 
---
+++ src/main/java/com/team766/robot/reva/procedures/ShootTrap.java
@@ -0,0 +1,44 @@
+package com.team766.robot.reva.procedures;
+
+import com.team766.robot.reva.Robot;
+import com.team766.robot.reva.VisionUtil.VisionPIDProcedure;
+import edu.wpi.first.math.geometry.Transform3d;
+import com.team766.ViSIONbase.AprilTagGeneralCheckedException;
+import com.team766.ViSIONbase.GrayScaleCamera;
+import com.team766.ViSIONbase.ScoringPosition;
+import com.team766.framework.Context;
+import com.team766.logging.LoggerExceptionUtils;
+import com.team766.robot.reva.VisionUtil.ScoringPositions;
+
+public class ShootTrap extends VisionPIDProcedure {
+	
+	int tagId;
+
+
+	public void run(Context context){
+		
+		while (true){
+			context.yield();
+			try{
+				tagId = getTagId();
+				break;
+			} catch (AprilTagGeneralCheckedException e){
+				continue;
+			}
+		}
+
+		for(int i = 0; i < ScoringPositions.trapScoringPositions.size(); i++){
+			if(ScoringPositions.trapScoringPositions.get(i).tagId == tagId){
+				new DriveToAndScoreAt(ScoringPositions.trapScoringPositions.get(i)).run(context);
+				break;
+			}
+		}
+		
+	}
+
+	private int getTagId() throws AprilTagGeneralCheckedException{
+		GrayScaleCamera toUse = Robot.forwardApriltagCamera.getCamera();
+
+		return toUse.getTagIdOfBestTarget();
+	}
+}
