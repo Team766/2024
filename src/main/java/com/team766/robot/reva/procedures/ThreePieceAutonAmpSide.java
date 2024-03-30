29c2f92f4360782b0d9b2a4c48b67a0a7dd5059e
qntmcube <100043837+qntmcube@users.noreply.github.com>
2024-03-30 11:14:45 -0700

    changed autons to old shootNow

--- src/main/java/com/team766/robot/reva/procedures/ThreePieceAutonAmpSide.java
+++ src/main/java/com/team766/robot/reva/procedures/ThreePieceAutonAmpSide.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class ThreePieceAutonAmpSide extends PathSequenceAuto {
     public ThreePieceAutonAmpSide() {
         super(Robot.drive, new Pose2d(0.75, 6.68, Rotation2d.fromDegrees(60)));
-        // addPath("3 Piece 1");
         addProcedure(new ShootAtSubwoofer());
         addProcedure(new AutoIntake());
         addPath("3 Piece 1");
