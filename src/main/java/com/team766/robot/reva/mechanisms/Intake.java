75f17c46b2c2588831e515cd7295092e2e0021cc
qntmcube <100043837+qntmcube@users.noreply.github.com>
2024-04-03 14:31:49 -0700

    moved prox sensor threshold into a constant we should increase to above 200

--- src/main/java/com/team766/robot/reva/mechanisms/Intake.java
+++ src/main/java/com/team766/robot/reva/mechanisms/Intake.java
@@ -35,6 +35,7 @@ public class Intake extends Mechanism {
     private static final double CURRENT_LIMIT = 30.0; // a little lower than max efficiency
     private static final double MAX_POWER = 1.0;
     private static final double MIN_POWER = -1 * MAX_POWER;
+    private static final double IS_CLOSE_THRESHOLD = 200;
 
     // This should be the amount that getRange() should return less than for a note to be classified
     // as in
@@ -111,7 +112,7 @@ public class Intake extends Mechanism {
     }
 
     public boolean isNoteClose() {
-        return (200) > sensor.getRange() && sensor.isRangeValid();
+        return (IS_CLOSE_THRESHOLD) > sensor.getRange() && sensor.isRangeValid();
     }
 
     public boolean hasNoteInIntake() {
