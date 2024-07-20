d21fe8ba3f6fd09ef0330143abb023c07ceda054
Driver Station Computer <rzghosh@gmail.com>
2024-07-19 23:00:22 -0700

    added autons to AutonomousMode

--- src/main/java/com/team766/robot/common/constants/ControlConstants.java
+++ src/main/java/com/team766/robot/common/constants/ControlConstants.java
@@ -3,7 +3,7 @@ package com.team766.robot.common.constants;
 public class ControlConstants {
 
     // Amount to reduce driving power to when holding the fine driving button
-    public static final double FINE_DRIVING_COEFFICIENT = 0.25;
+    public static final double FINE_DRIVING_COEFFICIENT = 0.4;
 
     // Value below which the joystick movement does not register
     public static final double JOYSTICK_DEADZONE = 0.05;
--- src/main/java/com/team766/robot/reva/AutonomousModes.java
+++ src/main/java/com/team766/robot/reva/AutonomousModes.java
@@ -4,7 +4,7 @@ import com.team766.framework.AutonomousMode;
 import com.team766.robot.reva.procedures.auton_routines.autons_v1.*;
 import com.team766.robot.reva.procedures.auton_routines.autons_v1.amp_side_auto.*;
 import com.team766.robot.reva.procedures.auton_routines.autons_v1.source_side_auto.*;
-import com.team766.robot.reva.procedures.auton_routines.autons_v1.middle_side_auto.*;
+import com.team766.robot.reva.procedures.auton_routines.autons_v2.*;
 
 public class AutonomousModes {
     public static final AutonomousMode[] AUTONOMOUS_MODES =
@@ -16,7 +16,7 @@ public class AutonomousModes {
                 // define one or more different autonomous modes with it like this:
                 //    new AutonomousMode("DriveFast", () -> new DriveStraight(1.0)),
                 //    new AutonomousMode("DriveSlow", () -> new DriveStraight(0.4)),
-                new AutonomousMode(
+                /* AutonomousMode(
                         "3p Start Amp, Amp and Center Pieces", () -> new ThreePieceAmpSide()),
                 new AutonomousMode("4p Start Amp, All Close Pieces", () -> new FourPieceAmpSide()),
                 new AutonomousMode(
@@ -31,7 +31,6 @@ public class AutonomousModes {
                 new AutonomousMode(
                         "4p Start Amp, All close pieces OG",
                         () -> new FourPieceAmpSide()),
-                new AutonomousMode("Just Shoot Amp", () -> new ThreePieceAmpSideCrazy()),
                 new AutonomousMode("3p Start Amp, Amp and Center Pieces, run away", () -> new Amp3NotesAutoncloseCrazy()),
                 new AutonomousMode("4p Start Amp, 1 Close and 2 Midfield Amp Side Pieces", () -> new Amp4NoteAuton1close1top1midfarCrazy()),
                 new AutonomousMode("4p Start Amp, 1 Close and 2 Midfield Amp Side and Under Stage Pieces", () -> new Amp4NoteAuton1close2farCrazy()),
@@ -40,5 +39,20 @@ public class AutonomousModes {
                 new AutonomousMode("5p center start", () -> new FivePieceMidStart()),
                 new AutonomousMode("4p close center start", () -> new FourPieceMidStart()),
                 new AutonomousMode("5p source start", () -> new FivePieceSourceSide()),
+                */
+                new AutonomousMode("Just Shoot Amp", () -> new JustShootAmp()),
+                new AutonomousMode("Just Shoot Source", () -> new JustShootSource()),
+                new AutonomousMode("Just Shoot Center", () -> new JustShootCenter()),
+                new AutonomousMode("3p Start Amp, 1,4", () -> new Amp14Auto()),
+                new AutonomousMode("3p Start Amp, 1,5 grab 4", () -> new Amp15Grab4Auto()),
+                new AutonomousMode("3p Start Amp, 1,5 pass 4", () -> new Amp15Pass4Auto()),
+                new AutonomousMode("4p Start Center, 1,2,3 grab 6", () -> new Center123Grab6Auto()),
+                new AutonomousMode("4p Start Center, 2,3,6", () -> new Center236Auto()),
+                new AutonomousMode("4p Start Center, 2,3,6 VISION", () -> new Center236VisionAuto()),
+                new AutonomousMode("5p Start Center, 1,2,3,5 VISION", () -> new Center1235VisionAuto()),
+                new AutonomousMode("5p Start Center, 1,2,3,6 VISION", () -> new Center1236VisionAuto()),
+                new AutonomousMode("2p Start Source, 8", () -> new Source8Auto()),
+                new AutonomousMode("2p Start Source, 8 grab 7", () -> new Source8Grab7Auto()),
+                new AutonomousMode("1p Start Source, BUMP MIDFIELD", () -> new SourceGrab8BumpMidAuto())
             };
 }
