fb6af9f155d16121d9490eb291a3f1df2d159adc
Debajit Ghosh <dg@dejabotz.com>
2024-09-05 14:23:52 -0700

    add some javadoc for Rule

--- src/main/java/com/team766/framework3/Rule.java
+++ src/main/java/com/team766/framework3/Rule.java
@@ -6,13 +6,62 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.BooleanSupplier;
 
+/**
+ * Rule to be evaluated in the {@link RuleEngine}.  Rules contain a
+ * "predicate" that will be evaluated in each call to {@link RuleEngine#run}, typically
+ * in an OperatorInterface loop or Display (LED lights, etc) loop.  The Rule keeps track of
+ * when the predicate starts triggering, continues triggering, and has finished triggering, via
+ * a {@link TriggerType}, eg when a driver or boxop starts pressing a button, continues holding down 
+ * the button, and releases the button.  Each Rule has optional {@link RunnableWithContext} actions
+ * for each of these trigger types, which the {@link RuleEngine} will consider running, after checking
+ * if higher priority rules have reserved the same {@link Mechanism}s that the candidate rule would use.
+ * 
+ * {@link Rule}s are always created and used with a {@link RuleEngine}.  Typically creation would be:
+ * 
+ * <pre>
+ * {@code
+ *   public class MyRules extends RuleEngine {
+ *     public MyRules() {
+ *       // add rule to spin up the shooter when the boxop presses the right trigger on the gamepad
+ *       rules.add(Rule.create(gamepad.getButton(InputConstants.XBOX_RT)).
+ *         withNewlyTriggeringRunnable(() -> new ShooterSpin(shooter)));
+ *       ...
+ *     }
+ * }
+ * </pre>  
+ */
 public class Rule {
 
+	/**
+	 * Functional interface for creating new {@link RunnableWithContext}s.
+	 */
     @FunctionalInterface
     public interface RunnableCreator {
         RunnableWithContext create();
     }
 
+	/**
+	 * Rules will be in one of four "trigger" (based on rule predicate) states:
+	 * 
+	 * NONE - rule is not triggering and was not triggering in the last evaluation.
+	 * NEWLY - rule just started triggering this evaluation.
+	 * CONTINUING - rule was triggering in the last evaluation and is still triggering.
+	 * FINISHED - rule was triggering in the last evaluation and is no longer triggering.
+	 *  
+	 */
+	enum TriggerType {
+        NONE,
+        NEWLY,
+        CONTINUING,
+        FINISHED
+    }
+
+	/**
+	 * Simple Builder for {@link Rule}s.  Configure Rules via this Builder; these fields will be immutable
+	 * in the rule the Builder constructs.
+	 * 
+	 * Instances of this Builder are created via {@link Rule#create} to simplify syntax.
+	 */
     public static class Builder {
         private BooleanSupplier predicate;
         private RunnableCreator newlyTriggeringRunnable;
@@ -20,25 +69,29 @@ public class Rule {
         private RunnableCreator finishedTriggeringRunnable;
 
         // TODO: make the newly triggering runnable a ctor argument as well?
-        public Builder(BooleanSupplier predicate) {
+        private Builder(BooleanSupplier predicate) {
             this.predicate = predicate;
         }
 
+		/** Specify a creator for the RunnableWithAction that should be run when this rule starts triggering. */
         public Builder withNewlyTriggeringRunnable(RunnableCreator action) {
             this.newlyTriggeringRunnable = action;
             return this;
         }
 
+		/** Specify a creator for the RunnableWithAction that should be run when this rule was triggering before and is continuing to trigger. */
         public Builder withContinuingTriggeringRunnable(RunnableCreator action) {
             this.continuingTriggeringRunnable = action;
             return this;
         }
 
+		/** Specify a creator for the RunnableWithAction that should be run when this rule was triggering before and is no longer triggering. */
         public Builder withFinishedTriggeringRunnable(RunnableCreator action) {
             this.finishedTriggeringRunnable = action;
             return this;
         }
 
+		// called by {@link RuleEngine#addRule}.
         /* package */ Rule build() {
             return new Rule(
                     predicate,
@@ -48,12 +101,7 @@ public class Rule {
         }
     }
 
-    enum TriggerType {
-        NONE,
-        NEWLY,
-        CONTINUING,
-        FINISHED
-    }
+
 
     private final BooleanSupplier predicate;
     private final Map<TriggerType, RunnableCreator> triggerRunnables =
@@ -62,6 +110,10 @@ public class Rule {
             Maps.newEnumMap(TriggerType.class);
 
     private TriggerType currentTriggerType = TriggerType.NONE;
+
+	public static Builder create(BooleanSupplier predicate) {
+		return new Builder(predicate);
+	}
 
     private Rule(
             BooleanSupplier predicate,
