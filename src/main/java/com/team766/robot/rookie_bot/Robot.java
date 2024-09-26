52df2f97856e4e50b0298231e80a0201b91751c2
Driver Station Computer <rzghosh@gmail.com>
2024-09-25 20:38:02 -0700

    rotation code

--- src/main/java/com/team766/robot/rookie_bot/Robot.java
+++ src/main/java/com/team766/robot/rookie_bot/Robot.java
@@ -4,13 +4,16 @@ import com.team766.framework.AutonomousMode;
 import com.team766.framework.Procedure;
 import com.team766.hal.RobotConfigurator;
 import com.team766.robot.example.mechanisms.*;
+import com.team766.robot.rookie_bot.mechanisms.Drive;
 
 public class Robot implements RobotConfigurator {
     // Declare mechanisms (as static fields) here
-
+    public static Drive drive;
+    
     @Override
     public void initializeMechanisms() {
         // Initialize mechanisms here
+        drive = new Drive();
     }
 
     @Override
---
+++ src/main/java/com/team766/robot/rookie_bot/procedures/DriveStraight.java
@@ -0,0 +1,13 @@
+package com.team766.robot.rookie_bot.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+import com.team766.robot.rookie_bot.Robot;
+
+public class DriveStraight extends Procedure {
+    public void run(Context context) {
+        Robot.drive.drive(0.05, 0.05);
+        context.waitForSeconds(3);
+        Robot.drive.drive(0, 0);
+    }
+}
