5ac4c96b66798b9f990c5d982f26769ca45ffee3
Ryan Cahoon <ryan.cahoon@zoox.com>
2024-10-01 03:02:47 -0700

    Delete old

--- src/main/java/com/team766/framework/ProcedureWithValue.java
+++
@@ -1,4 +0,0 @@
-package com.team766.framework;
-
-public abstract class ProcedureWithValue<T> extends ProcedureBase
-        implements RunnableWithContextWithValue<T> {}
--- src/main/java/com/team766/framework/RunnableWithContext.java
+++
@@ -1,6 +0,0 @@
-package com.team766.framework;
-
-@FunctionalInterface
-public interface RunnableWithContext {
-    void run(Context context);
-}
