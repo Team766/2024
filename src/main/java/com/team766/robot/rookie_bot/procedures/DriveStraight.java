441bdc25d2dfd8e56ba8d83d6880ed3939e5f797
qntmcube <100043837+qntmcube@users.noreply.github.com>
2024-10-19 14:10:29 -0700

    copied programming rotation code into correct branch

---
+++ src/main/java/com/team766/robot/rookie_bot/procedures/DriveStraight.java
@@ -0,0 +1,17 @@
+package com.team766.robot.rookie_bot.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.rookie_bot.Robot;
+
+public class DriveStraight extends Procedure {
+
+    	public void run(final Context context) {
+		Robot.drive.setDriverPower(0.5, 0.5);
+
+		context.waitForSeconds(2)
+	
+		Robot.drive.setDrivePower(0,0)
+
+	}	
+}
