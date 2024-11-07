440205e2660b542664a5bead3e5f46e5bc74ca28
raghavsial <raghav.sial21@gmail.com>
2024-11-07 04:03:24 +0000

    drivecontrol

---
+++ src/main/java/com/team766/robot/rookie_bot/mechanisms/Launcher.java
@@ -0,0 +1,11 @@
+package com.team766.robot.mechanisms;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.SolenoidController;
+import com.team766.hal.RobotProvider;
+
+public class Launcher extends Mechanism{
+private SolenoidController pusher;
+
+public Launcher()
+}
