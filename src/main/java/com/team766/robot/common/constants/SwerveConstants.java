a9ab5dee7c4bff059b0abaa69d8a0328634e62b4
qntmcube <100043837+qntmcube@users.noreply.github.com>
2024-11-15 22:05:42 -0800

    fixed stupid steer gear ratio bug

--- src/main/java/com/team766/robot/common/constants/SwerveConstants.java
+++ src/main/java/com/team766/robot/common/constants/SwerveConstants.java
@@ -8,17 +8,17 @@ public final class SwerveConstants {
     // FIXME: have these be passed in from Drive (via SwerveConfig).  Many of these are
     // passed into Odometry.  (Swerve and Odometry code need to be renconciled.)
 
+    public static final double DRIVE_GEAR_RATIO = 6.75; // L2 gear ratio configuration
+
+    public static final double STEER_GEAR_RATIO = 150.0 / 7.0; // L2 gear ratio configuration
+
     /*
      * Factor that converts between motor rotations and wheel degrees
      * Multiply to convert from wheel degrees to motor rotations
      * Divide to convert from motor rotations to wheel degrees
      */
     public static final double ENCODER_CONVERSION_FACTOR =
-            (150.0 / 7.0) /*steering gear ratio*/ * (1. / 360.0) /*degrees to motor rotations*/;
-
-    public static final double DRIVE_GEAR_RATIO = 6.75; // L2 gear ratio configuration
-
-    public static final double STEER_GEAR_RATIO = 150/7; // L2 gear ratio configuration
+            STEER_GEAR_RATIO /*steering gear ratio*/ * (1. / 360.0) /*degrees to motor rotations*/;
 
     // Radius of the wheels. The circumference was measured to be 30.5cm, then experimentally this
     // value had
