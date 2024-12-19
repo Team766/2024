7d1c6f062cee877e5c33b641e7465739bae815c0
Driver Station Computer <rzghosh@gmail.com>
2024-12-18 19:44:55 -0800

    elevator led

--- src/main/java/com/team766/robot/candle_bot/mechanisms/Elevator.java
+++ src/main/java/com/team766/robot/candle_bot/mechanisms/Elevator.java
@@ -3,19 +3,30 @@ package com.team766.robot.candle_bot.mechanisms;
 import com.team766.framework.Mechanism;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
+import com.team766.hal.wpilib.CANSparkMaxMotorController;
+import com.team766.robot.candle_bot.Robot;
+import com.team766.robot.candle_bot.Robot;
 
 public class Elevator extends Mechanism{
-    private MotorController elevatormotor;
+    private CANSparkMaxMotorController elevatormotor;
+    private double targetPos; 
 
 public Elevator(){
-    elevatormotor = RobotProvider.instance.getMotor("Elevator.elevatormotor");
+    elevatormotor = (CANSparkMaxMotorController) RobotProvider.instance.getMotor("Elevator.elevatormotor");
+    elevatormotor.setSmartCurrentLimit(10, 80, 200);
     elevatormotor.setSensorPosition(0);
 }
     public void setPosition(double position) {
         checkContextOwnership();
+        targetPos = position;
         elevatormotor.set(MotorController.ControlMode.Position, position);
     }
     public void run(){
+        if (Math.abs(elevatormotor.getSensorPosition() - targetPos) < 1){
+            Robot.candle.LED();
+        } else {
+            Robot.candle.stop();
+        }
         log("elevator encoder: " + elevatormotor.getSensorPosition());
     }
     
